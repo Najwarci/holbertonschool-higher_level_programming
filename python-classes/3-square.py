9689f882be44e6fadccd555897def3a6c7eea47b
Najwarci <rachdi.najwa91@gmail.com>
2025-05-22 19:54:32 +0200

    area

---
+++ python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines my square with private size and a method to compute"""
+
+
+class Square:
+    """Class that defines my square with a size"""
+
+    def __init__(self, size=0):
+        """Initializes square with optional size and validates it"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns square area"""
+        return self.__size ** 2
