421cafbaa63b3b3bf859810c776d74d59e8f232e
Najwarci <rachdi.najwa91@gmail.com>
2025-05-23 16:17:53 +0200

    Coordinates of a square

---
+++ python-classes/6-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Defines my square with size, position and a method to compute and print"""
+
+
+class Square:
+    """Class that defines my square with a size and a position"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes square with optional size and position, and validates them"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Returns the current size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square with type and value verification"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Returns the current position of the square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position of the square with type and value verification"""
+        if (not isinstance(value, tuple) or
+            len(value) != 2 or
+            not all(isinstance(n, int) for n in value) or
+            not all(n >= 0 for n in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with '#' considering the position offset"""
+        if self.__size == 0:
+            prin
