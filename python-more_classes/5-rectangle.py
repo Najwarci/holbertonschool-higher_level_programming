9dac004ca3f0c51fc1465b381cf8e56df8440292
Najwarci <rachdi.najwa91@gmail.com>
2025-05-23 23:04:54 +0200

    task 5

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle with width and height validation."""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle instance."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width with validation."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height."""
+        return self.__hei
