d30918a411d148903a262a3d6a574f5ddb0a0a4f
Najwarci <rachdi.najwa91@gmail.com>
2025-06-01 18:02:15 +0200

    correction

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,58 +1,39 @@
 #!/usr/bin/env python3
 
-import math
-from abc import ABC, abstractmethod
+"""
+Mixin that adds swimming ability
+"""
 
 
-def shape_info(shape):
-    """
-    Prints the area and perimeter of a shape.
-    Assumes the object has area() and perimeter() methods.
-    """
-    print("Area: {}".format(shape.area()))
-    print("Perimeter: {}".format(shape.perimeter()))
+class SwimMixin:
+    def swim(self):
+        """
+        Print a message indicating the creature can swim
+        """
+        print("The creature swims!")
 
 
-class Shape(ABC):
-    """
-    Abstract base class for shapes.
-    """
-
-    @abstractmethod
-    def area(self):
-        pass
-
-    @abstractmethod
-    def perimeter(self):
-        pass
+"""
+Mixin that adds flying ability
+"""
 
 
-class Circle(Shape):
-    """
-    Circle shape.
-    """
-
-    def __init__(self, radius):
-        self.radius = abs(radius)
-
-    def area(self):
-        return math.pi * (self.radius ** 2)
-
-    def perimeter(self):
-        return 2 * math.pi * self.radius
+class FlyMixin:
+    def fly(self):
+        """
+        Print a message indicating the creature can fly
+        """
+        print("The creature flies!")
 
 
-class Rectangle(Shape):
-    """
-    Rectangle shape.
-    """
+"""
+Dragon class that inherits both swimming and flying abilities
+"""
 
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
 
-    def area(self):
-        return self.width * self.height
-
-    def perimeter(self):
-        return 2 * (self.width + self.height)
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        """
+        Print a message indicating the dragon can roar
+        """
+        print("The dragon roars!")
