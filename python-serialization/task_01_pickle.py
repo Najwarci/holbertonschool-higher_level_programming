b304d0be5100ee32b511c1b1b82ef5c572fc60f7
Najwarci <rachdi.najwa91@gmail.com>
2025-06-05 15:55:44 +0200

    correction

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -10,7 +10,7 @@ class CustomObject:
 
     def display(self):
         print(f"Name: {self.name}")
-        print(f"Age: {self.Age}")
+        print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
